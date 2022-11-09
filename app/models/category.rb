bb0b39e99865918168e1e0788a31f519087c0d84
Alexander <alexander@MBP-Alexander.netis>
2022-11-09 13:17:22 +0300

    Some pages

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,9 @@
 class Category < ApplicationRecord
   include Tree
   has_many :documents, dependent: :nullify
-
   validates :slug, uniqueness: true
+
+  def url
+    "/c/#{parents.any? ? parents.map(&:slug).join("/") + '/' : ''}#{slug}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  NESTED_LEVEL = 4 # nested level of categories url
+
+
   root 'pages#main'
   get 'admin', to: 'admin/pages#main'
   namespace :admin do
@@ -10,6 +14,14 @@ Rails.application.routes.draw do
     resources :categories
     resources :documents
     resources :files
+  end
+
+  get "/c", to: "categories#index"
+
+  (1..NESTED_LEVEL).to_a.each do |level|
+      categories_part = (1..level).to_a.map { |num| ":c_#{num}"}.join("/")
+      get "/c/#{categories_part}/document/:slug", to: "documents#show"
+      get "/c/#{categories_part}", to: "categories#show"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
